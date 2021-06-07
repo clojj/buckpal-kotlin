8cc9fa032b2549aac81c3df8e6417b2a3042bd63
jw <jwin1968@gmail.com>
2021-06-07 08:57:16 +0200

    wip

--- src/main/kotlin/io/reflectoring/buckpal/account/application/port/inbound/SendMoneyUseCase.kt
+++ src/main/kotlin/io/reflectoring/buckpal/account/application/port/inbound/SendMoneyUseCase.kt
@@ -1,9 +1,6 @@
 package io.reflectoring.buckpal.account.application.port.inbound
 
-import arrow.core.ValidatedNel
-import arrow.core.invalidNel
-import arrow.core.validNel
-import arrow.core.zip
+import arrow.core.*
 import java.math.BigInteger
 
 interface SendMoneyUseCase {
@@ -18,12 +15,11 @@ class SendMoneyCommand private constructor(
 
     companion object {
         fun of(sourceAccountId: Long?, targetAccountId: Long?, amount: BigInteger?) =
-            sourceAccountId.validateAsAccountId()
-                .zip(
-                    targetAccountId.validateAsAccountId(),
-                    amount.validate()
-                )
-                { source, target, amount -> SendMoneyCommand(source, target, amount) }
+            allValidations(
+                sourceAccountId.validateAsAccountId(),
+                targetAccountId.validateAsAccountId(),
+                amount.validate()
+            ) { source, target, amount -> SendMoneyCommand(source, target, amount) }
 
         private fun Long?.validateAsAccountId(): ValidatedNel<String, Long> =
             this?.validNel() ?: "null".invalidNel()
@@ -32,5 +28,24 @@ class SendMoneyCommand private constructor(
             this?.let { if (it >= BigInteger.TEN) it.validNel() else "must be at least 10 bucks".invalidNel() }
                 ?: "null".invalidNel()
 
+    }
+}
+
+fun <E, A, B, C, R> allValidations(
+    validated1: ValidatedNel<E, A>,
+    validated2: ValidatedNel<E, B>,
+    validated3: ValidatedNel<E, C>,
+    f: (A, B, C) -> R
+) =
+    validated1.zip(validated2, validated3) { a, b, c -> f(a, b, c) }
+
+
+fun <ERROR, Any> zipValidatedNel(validated: Nel<ValidatedNel<ERROR, Any>>, f: (Nel<Any>) -> Any) {
+    if (validated.size > 10) TODO()
+    else with(validated) {
+        when (size) {
+            3 -> first().zip(this[1]) { a, b -> f(Nel(a, listOf(b))) }
+            else -> TODO()
+        }
     }
 }
